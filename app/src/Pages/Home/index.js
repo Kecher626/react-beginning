09fbdafab6c7b29869bf6af918068685120def74
Kecher626 <stichi410714@gmail.com>
2021-05-28 00:24:28 +0800

    Components inittial & import

---
+++ app/src/Pages/Home/index.js
@@ -0,0 +1,13 @@
+import Edit from "./Components/Edit"
+import Item from "./Components/Item"
+import List from "./Components/List"
+import "./index.css"
+
+const Home = ()=>{
+    return <div className="app">
+        <Edit />
+        <List />
+    </div>
+}
+
+export default Home
